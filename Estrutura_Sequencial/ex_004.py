00360096efdae33fc8cd5d6709e3e9229b261b96
Pablo <pablocavalcantee@gmail.com>
2026-04-14 22:14:37 -0300

    add ex_004

--- Estrutura_Sequencial/ex_004.py
+++ Estrutura_Sequencial/ex_004.py
@@ -1,1 +1,8 @@
 # Faça um programa que peça as 4 notas bimestrais e mostre a média.
+total = []
+
+for i in range(4):
+    notas = float(input(f'Digite a {i + 1} nota: '))
+    total.append(notas)
+media = sum(total) / 4
+print(f'A média foi de {media:.2f}')        
