481a834510a52ee867048aa46473216b2eb428c9
Pablo <pablocavalcantee@gmail.com>
2026-04-14 21:33:42 -0300

    criação do projeto

---
+++ Estrutura_Sequencial/ex_003.py
@@ -0,0 +1,5 @@
+# Faça um programa que peça dois números e imprima a soma:
+
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+print(f'A soma entre {num1} + {num2} é igual a {num1 + num2}')
---
+++ Estrutura_Sequencial/ex_004.py
@@ -0,0 +1,1 @@
+# Faça um programa que peça as 4 notas bimestrais e mostre a média.
